83594fd72556dc8f36354b7a412a431efabedd90
benekin <beniamin.bartusiak@gmail.com>
2019-11-07 20:55:26 +0100

    fetch Riot API

--- main.go
+++ main.go
@@ -8,6 +8,28 @@ import (
 	"os"
 )
 
+// Summoner represents summoner data returned from Riot API
+type Summoner struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	IconID int    `json:"profileIconId"`
+}
+
+// Champion represents champion data returned from Riot API
+type Champion struct {
+	Level        int  `json:"championLevel"`
+	ChestGranted bool `json:"chestGranted"`
+	Points       int  `json:"championPoints"`
+	ChampionID   int  `json:"championId"`
+	LastPlayTime int  `json:"lastPlayTime"`
+}
+
+// SummonerWithChampion represents summoner data combined with his champions
+type SummonerWithChampion struct {
+	Summoner
+	Champions []Champion `json:"champions"`
+}
+
 func myHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("Hello World!"))
 	if err != nil {
@@ -33,9 +55,40 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(summonerName)
+	riotAPISummonerNameURL := "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName
+	body, err = fetchRiotAPI(riotAPISummonerNameURL)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-	output, err := json.Marshal("Hello " + summonerName)
+	var summoner Summoner
+	err = json.Unmarshal(body, &summoner)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	riotAPIChampionMasteryURL := "https://eun1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + summoner.ID
+	body, err = fetchRiotAPI(riotAPIChampionMasteryURL)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var champions []Champion
+	err = json.Unmarshal(body, &champions)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	output, err := json.Marshal(SummonerWithChampion{summoner, champions})
+
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -50,6 +103,27 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func fetchRiotAPI(riotAPISummonerNameURL string) ([]byte, error) {
+	req, err := http.NewRequest("GET", riotAPISummonerNameURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Riot-Token", "RGAPI-cf346092-22cd-4ad2-9210-70157271ec76")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
 
 func main() {
