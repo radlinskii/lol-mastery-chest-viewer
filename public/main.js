6df3efc9964ed32b92089ee7bb3d6664676515cc
benekin <beniamin.bartusiak@gmail.com>
2019-11-09 13:55:37 +0100

    display champion information

--- public/main.js
+++ public/main.js
@@ -1,5 +1,12 @@
 /* eslint-disable no-console */
 
+async function fetchChampionsJSON() {
+    const response = await fetch('champions.json');
+    const json = await response.json();
+
+    return json
+}
+
 async function submitHandler(event) {
     event.preventDefault();
     const inputValue = event.target[0].value;
@@ -13,7 +20,17 @@ async function submitHandler(event) {
         if (response.ok) {
             const responseBody = await response.json();
 
-            console.log(responseBody);
+            const championsJSON = await fetchChampionsJSON();
+            const championsMap = parseChampionsJSON(championsJSON);
+            const championsData = mergeChampionsData(championsMap, responseBody);
+
+            const summonerWithChampions = {
+                name: responseBody.name,
+                profileIconId: responseBody.profileIconId,
+                champions: championsData,
+            };
+
+            addResults(summonerWithChampions);
         } else {
             console.error(response);
         }
@@ -21,6 +38,47 @@ async function submitHandler(event) {
         console.error('fetching error');
         console.error(error);
     }
+}
+
+function mergeChampionsData(championsMap, summoner) {
+    const championsData = summoner.champions.map((champ) => ({
+        ...champ,
+        ...championsMap[champ.championId]
+    }));
+
+    return championsData;
+}
+
+function parseChampionsJSON(championsJSON) {
+    const champions = Object.values(championsJSON.data);
+    const championsByID = champions.reduce((acc, champ) => ({
+            ...acc,
+            [champ.key]: champ,
+        }), {});
+
+    return championsByID;
+}
+
+function addResults(summoner) {
+    const container = document.getElementById('container');
+
+    const summonerGreeting = document.createElement('h2');
+    summonerGreeting.innerText = `Hello ${summoner.name}`;
+    container.appendChild(summonerGreeting);
+
+    summoner.champions.forEach(champion => {
+        const div = document.createElement('div');
+
+        const championName = document.createElement('span');
+        championName.innerText = champion.name;
+        div.appendChild(championName);
+
+        const championChest = document.createElement('span');
+        championChest.innerText = ` - chest ${champion.chestGranted ?  'already granted' : 'available'}`;
+        div.appendChild(championChest);
+
+        container.appendChild(div);
+    });
 }
 
 window.onload = function onLoad() {
