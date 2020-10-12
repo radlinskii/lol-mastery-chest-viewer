a86980dd7286613cfba5d5f7e69012f2d56de23f
radlinskii <radlinskiignacy@gmail.com>
2020-10-12 22:47:38 +0200

    add build to temporarly fix deployment

---
+++ client/build/precache-manifest.5de43faf4498b5e11c26e9bde34b256a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21090775a7e730c6f6d193279b052a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bbf60a7eb2fd57ed41a",
+    "url": "/static/css/main.3f5e96c5.chunk.css"
+  },
+  {
+    "revision": "9da596310a3bab2cf4dd",
+    "url": "/static/js/2.6cfa13ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6cfa13ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbf60a7eb2fd57ed41a",
+    "url": "/static/js/main.fc1b051a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
