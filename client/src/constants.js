abc3decbe0a46f0fb3e8ef0f245455f9850eb85c
radlinskii <radlinskiignacy@gmail.com>
2020-10-14 18:24:25 +0200

    refactor development environment

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,4 @@
 export const SUMMONER_SEARCH_PARAM = 'summonerName';
 export const PATCH_VERSION = '10.20.1';
 export const D_DRAGON_URL = `https://ddragon.leagueoflegends.com/cdn/${PATCH_VERSION}`;
+export const API_URL = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL : '';
