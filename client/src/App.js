115a9a9f64ce41e6b4e20b718b4a475bffb06409
Radliński Ignacy <radlinskiignacy@gmail.com>
2020-10-21 23:19:25 +0200

    code refactor (#44)
    * add logo
    
    * code refactor
    
    * install prop-types

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import HomePage from './components/HomePage';
 import { SUMMONER_SEARCH_PARAM } from './constants';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	logo: {
+		position: 'relative',
+		top: '10px',
+		height: '64px',
+		width: '64px',
+		paddingRight: '16px',
+	},
+	header: {
+		marginTop: '32px',
+		marginBottom: '32px',
+		display: 'flex',
+		alignItems: 'center',
+	},
+	wrapper: {
+		marginLeft: '40px',
+		display: 'inline-block',
+	},
+	homeLink: {
+		textDecoration: 'none',
+		color: theme.palette.text.primary,
+	},
+}));
 
 function App() {
+	const classes = useStyles();
 	const currentParams = new URLSearchParams(window.location.search);
 	const summonerName = currentParams.get(SUMMONER_SEARCH_PARAM);
 
 	return (
-		<Container >
-			<Typography variant="h2" component="h1">
-				LoL Mastery Chest Viewer
-			</Typography>
+		<Container>
+			<Box className={classes.header}>
+				<Box className={classes.wrapper}>
+					<a href={window.location.origin} className={classes.homeLink}>
+						<img alt="LoL Mastery Chest Viewer" className={classes.logo} src="https://raw.githubusercontent.com/radlinskii/lol-mastery-chest-viewer/master/docs/images/logo.png"/>
+						<Typography variant="h2" component="h1" display="inline" className={classes.title}>
+							LoL Mastery Chest Viewer
+						</Typography>
+					</a>
+				</Box>
+			</Box>
 			<HomePage value={summonerName ?? ''}/>
 		</Container>
 	);
--- client/src/components/EnhancedTableHead/EnhancedTableHead.js
+++ client/src/components/EnhancedTableHead/EnhancedTableHead.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import {
     TableHead,
     TableRow,
@@ -14,12 +14,12 @@ const useStyles = makeStyles({
     },
     visuallyHidden: {
         border: 0,
-        clip: "rect(0 0 0 0)",
+        clip: 'rect(0 0 0 0)',
         height: 1,
         margin: -1,
-        overflow: "hidden",
+        overflow: 'hidden',
         padding: 0,
-        position: "absolute",
+        position: 'absolute',
         top: 20,
         width: 1
     }
@@ -44,9 +44,9 @@ export default function EnhancedTableHead(props) {
             <TableRow>
                 {headers.map((headCell) => {
                     if (headCell.hasOwnProperty('noSort')) {
-                        return <TableCell key={headCell.id} className={classes.tableHeadCell} align={headCell.align}>{headCell.label}</TableCell>
-                    }
-                    else {
+                        return <TableCell key={headCell.id} className={classes.tableHeadCell}
+                                          align={headCell.align}>{headCell.label}</TableCell>;
+                    } else {
                         return <TableCell
                             key={headCell.id}
                             className={classes.tableHeadCell}
@@ -55,17 +55,17 @@ export default function EnhancedTableHead(props) {
                         >
                             <TableSortLabel
                                 active={orderBy === headCell.id}
-                                direction={orderBy === headCell.id ? order : "asc"}
+                                direction={orderBy === headCell.id ? order : 'asc'}
                                 onClick={createSortHandler(headCell.id)}
                             >
                                 {headCell.label}
                                 {orderBy === headCell.id ? (
                                     <span className={classes.visuallyHidden}>
-                                        {order === "desc" ? "sorted descending" : "sorted ascending"}
+                                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                                     </span>
                                 ) : null}
                             </TableSortLabel>
-                        </TableCell>
+                        </TableCell>;
                     }
                 })}
             </TableRow>
@@ -76,7 +76,7 @@ export default function EnhancedTableHead(props) {
 EnhancedTableHead.propTypes = {
     headers: PropTypes.arrayOf(PropTypes.object).isRequired,
     onRequestSort: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
     orderBy: PropTypes.string.isRequired
 };
 
